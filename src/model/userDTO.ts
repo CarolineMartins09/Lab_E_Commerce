3fd2c49d29f0147e0365288563b5e2b9052c15a2
carolinemartins <caroline.martins.soares09@gmail.com>
2023-01-24 14:47:08 -0300

    controller user

---
+++ src/model/userDTO.ts
@@ -0,0 +1,12 @@
+export interface InputUserDTO{
+    name:string,
+    email:string, 
+    password:string
+}
+
+export interface CreateUserDTO{
+    id:string,
+    name:string,
+    email:string,
+    password:string
+}
